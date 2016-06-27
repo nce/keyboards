3bcaa5a6bdf44c1217892bb1659ebc6d0fdface3
Ulli Goschler <ulligoschler@gmail.com>
2016-06-27 10:30:21 +0200

    init

---
+++ gh60-hx/README.md
@@ -0,0 +1,114 @@
+GH60 Custom Layout
+==================
+
+This document describes my custom firmware which I used for the fully
+programmable [GH60 china clone](docs/gh60pcb.jpg) (hx60) PCB.
+
+
+![GH60 China clone hx60 dsa sublimated][docs/gh60.jpg]
+
+The displayed keyboard was assembled with:
+  * GH60 China Clone hx60
+  * Cherry stabilizers
+  * Cherry MX-Clear switches
+  * Black 60% plastic-mold case
+  * Signature Plastics DSA pbt sublimated series (GDE & WAN)
+
+## Pok3r
+
+The pok3r layout provided by tmk, does not represent the *original* layout.
+My [keymap](keymap_pokerorg.c) is a more native implementation of the *pok3r*.
+
+### Default Layer
+
+*A keycode table is available [here](https://github.com/tmk/tmk_keyboard/blob/master/tmk_core/doc/keycode.txt)*
+
+```
+,-----------------------------------------------------------.
+|Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
+|-----------------------------------------------------------|
+|Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
+|-----------------------------------------------------------|
+|Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
+|-----------------------------------------------------------|
+|Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
+|-----------------------------------------------------------|
+|Ctrl|Gui |Alt |       Space             |Alt |FN |Gui |Ctrl|
+`-----------------------------------------------------------'
+```
+
+By pressing <kbd>Menu</kbd> (FN) you can switch to the `function layer`.
+
+
+### Function Layer
+
+```
+,-------------------------------------------------------------.
+|Grv| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Del     |
+|-------------------------------------------------------------|
+|FnQ |Mprv|Mply|Mnxt| | |Calc|Pgup|Up |Pgdn|Pscr|Slck|Paus|FnL|
+|-------------------------------------------------------------|
+|      |   |VolD|VolU|Mute|  |Home|Left|Down|Rght|Ins|Del|    |
+|-------------------------------------------------------------|
+|        |App|   |   |   |   |End|   |   |    |    |          |
+|-------------------------------------------------------------|
+|    |    |    |           FnS            |    |    |    |    |
+`-------------------------------------------------------------'
+```
+> FnS: toggle Arrow overlay
+> FnQ: toggle GRV overlay
+> FnL: to Layout selector overlay
+
+The **arrow**-overlay transforms the bottom right side of the Keyboard into an
+arrow cluster:
+
+```
+                       ;|  '|Return  |
+                 --------------------|
+                 ,|  .|  /|   Up     |
+          ---------------------------|
+Space           |Alt |Left|Down|Right|
+-------------------------------------'
+```
+
+The **Grv**-overlay transforms <kbd>ESC<kbd> to `` ` ``
+
+### Alternative Layouts
+As a custom Keyboard is usually lacking Dip-switches, we need another
+method to switch layouts.
+
+Therefore a layout selector layer is introduced by pressing
+<kbd>FN</kbd>+<kbd>\<kbd> **and** one of the following keys simultaneously:
+
+```
+,-----------------------------------------------------------.
+| Lq| Lc| Ld| Lw|   |   |   |   |   |   |   |   |   |       |
+|-----------------------------------------------------------|
+|     |Lq |Lw |   |   |   |   |   |   |   |   |   |   |     |
+|-----------------------------------------------------------|
+|      |   |   |Ld |   |   |   |   |   |   |   |   |        |
+|-----------------------------------------------------------|
+|        |   |   |Lc |   |   |   |   |   |   |   |          |
+|-----------------------------------------------------------|
+|    |    |    |                        |    |    |    |    |
+`-----------------------------------------------------------'
+```
+> Lq: QWERTY layout
+> Lc: Colemak layout
+> Ld: Dvorak layout
+> Lw: Workman layout
+
+This is a permanet switch, until it is manually "reversed" to QWERTY via
+<kbd>Menu</kbd>+<kbd>\</kbd>+<kbd>ESC</kbd>
+
+## HHKB
+
+My *happy hacking 60%*-[layout](keymap_hh60.c) was a temporary solution to simulate a kind of
+HHKB layout on the classic 60% keyboard (mostly by mapping `ESC` to `Capslock`
+and switching `Backspace` with `\`).
+
+It features a `vim` styled arrow cluster (`hjkl` movement) and a long pressed
+<kbd>;<kbd> function layer.
+
+*Atm the source codes comments dont reflect the actual code*
+
---
+++ gh60-hx/keymap_pokerorg.c
@@ -0,0 +1,104 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: qwerty */
+    KEYMAP_ANSI(
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
+        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \
+        LCTL,LGUI,LALT,          SPC,                     RALT, FN0,RGUI,RCTL),
+    /* 1: colemak */
+    KEYMAP_ANSI(
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
+        TAB, Q,   W,   F,   P,   G,   J,   L,   U,   Y,   SCLN,LBRC,RBRC,BSLS, \
+        BSPC,A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,     ENT,  \
+        LSFT,Z,   X,   C,   V,   B,   K,   M,   COMM,DOT, SLSH,          RSFT, \
+        LCTL,LGUI,LALT,          SPC,                     RALT, FN0,RGUI,RCTL),
+    /* 2: dvorak */
+    KEYMAP_ANSI(
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   LBRC,RBRC,BSPC, \
+        TAB, QUOT,COMM,DOT, P,   Y,   F,   G,   C,   R,   L,   SLSH,EQL, BSLS, \
+        CAPS,A,   O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,     ENT,  \
+        LSFT,SCLN,Q,   J,   K,   X,   B,   M,   W,   V,   Z,             RSFT, \
+        LCTL,LGUI,LALT,          SPC,                     RALT, FN0,RGUI, RCTL),
+    /* 3: workman */
+    KEYMAP_ANSI(
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
+        TAB, Q,   D,   R,   W,   B,   J,   F,   U,   P,   SCLN,LBRC,RBRC,BSLS, \
+        BSPC,A,   S,   H,   T,   G,   Y,   N,   E,   O,   I,   QUOT,     ENT,  \
+        LSFT,Z,   X,   M,   C,   V,   K,   L,   COMM,DOT, SLSH,          RSFT, \
+        LCTL,LGUI,LALT,          SPC,                     RALT, FN0,RGUI, RCTL),
+    /* 4: Poker with Arrow */
+    KEYMAP_ANSI(
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          UP,   \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,LEFT,DOWN,RGHT),
+    /* 5: Poker with Grv instead of ESC*/
+    KEYMAP_ANSI(
+        GRV , TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+    /* 6: Poker Fn
+     * ,-------------------------------------------------------------.
+     * |Grv| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Del     |
+     * |-------------------------------------------------------------|
+     * |FnQ |Mprv|Mply|Mnxt| | |Calc|Pgup|Up |Pgdn|Pscr|Slck|Paus|Fnl|
+     * |-------------------------------------------------------------|
+     * |      |   |VolD|VolU|Mute|  |Home|Left|Down|Rght|Ins|Del|    |
+     * |-------------------------------------------------------------|
+     * |        |App|   |   |   |   |End|   |   |    |    |          |
+     * |-------------------------------------------------------------|
+     * |    |    |    |           FnS            |    |    |    |    |
+     * `-------------------------------------------------------------'
+     * Fn:  to Fn overlay
+     * FnS: toggle Arrow overlay
+     * FnQ: toggle Esc overlay
+     * FnL: to Layout selector overlay
+     */
+    KEYMAP_ANSI(
+        GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL, \
+        FN2,MPRV,MPLY,MNXT,TRNS,TRNS,CALC,PGUP,  UP,PGDN,PSCR,SLCK,PAUS,FN4, \
+        TRNS,TRNS,VOLD,VOLU,MUTE,TRNS,HOME,LEFT,DOWN,RGHT,INS,DEL,    TRNS, \
+        TRNS,APP ,TRNS,TRNS,TRNS,TRNS,END ,TRNS,TRNS,TRNS,TRNS,         TRNS, \
+        TRNS,TRNS,TRNS,          FN1,                     TRNS,TRNS,TRNS,TRNS),
+    /* 7: Layout selector
+     * ,-----------------------------------------------------------.
+     * | Lq| Lc| Ld| Lw|   |   |   |   |   |   |   |   |   |       |
+     * |-----------------------------------------------------------|
+     * |     |Lq |Lw |   |   |   |   |   |   |   |   |   |   |     |
+     * |-----------------------------------------------------------|
+     * |      |   |   |Ld |   |   |   |   |   |   |   |   |        |
+     * |-----------------------------------------------------------|
+     * |        |   |   |Lc |   |   |   |   |   |   |   |          |
+     * |-----------------------------------------------------------|
+     * |    |    |    |                        |    |    |    |    |
+     * `-----------------------------------------------------------'
+     * Lq: set Qwerty layout
+     * Lc: set Colemak layout
+     * Ld: set Dvorak layout
+     * Lw: set Workman layout
+     */
+    KEYMAP_ANSI(
+        FN5, FN6, FN7, FN8, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,FN5, FN8, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,FN7, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,FN6, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+};
+const uint16_t PROGMEM fn_actions[] = {
+    /* Poker Layout */
+    [0] = ACTION_LAYER_MOMENTARY(6),  // to Fn overlay
+    [1] = ACTION_LAYER_TOGGLE(4),     // toggle arrow overlay
+    [2] = ACTION_LAYER_TOGGLE(5),     // toggle Esc overlay
+    [3] = ACTION_MODS_KEY(MOD_RCTL|MOD_RSFT, KC_ESC), // Task(RControl,RShift+Esc)
+    [4] = ACTION_LAYER_MOMENTARY(7),  // to Layout selector
+    [5] = ACTION_DEFAULT_LAYER_SET(0),  // set qwerty layout
+    [6] = ACTION_DEFAULT_LAYER_SET(1),  // set colemak layout
+    [7] = ACTION_DEFAULT_LAYER_SET(2),  // set dvorak layout
+    [8] = ACTION_DEFAULT_LAYER_SET(3),  // set workman layout
+};
