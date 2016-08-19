d28fcfacb4ed8c1bf3f66fbed7a5a2ffdeebc000
Ulli Goschler <ulligoschler@gmail.com>
2016-08-19 10:33:44 +0200

    remove space cadent, as i mostly pressed them accidentally; instead put a open/closing parenthesis on the right/left outter key

--- ergodox/nce/keymap.c
+++ ergodox/nce/keymap.c
@@ -15,10 +15,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '    |
- * |--------+------+------+------+------+------| Lbkt |           | Rbkt |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * |--------+------+------+------+------+------|   [  |           |   ]  |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |   {  |           |   }  |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Lgui | Lgui |  `~  |  '"  | Lgui |                                       | Tab  | Down |   [  |   ]  | Rgui |
+ *   |  (   |      |  `~  |  '"  | Lgui |                                       | Tab  |      |   [  |   ]  |  )   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,--------------.
  *                                        | Del  | Alt  |       | Alt |Ctrl/Esc|
@@ -35,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,     KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_EQL,
         KC_TAB,     KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   MO(SYMB),
         KC_LCTL,    KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSPO,    KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
-        KC_LGUI,    KC_LGUI,      KC_GRV, KC_QUOT,KC_LGUI,
+        KC_LSFT,    KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
+        LSFT(KC_9),    KC_TRNS,      KC_GRV, KC_QUOT,KC_LGUI,
                                                         KC_DEL, KC_LALT,
                                                               KC_HOME,
                                                     KC_BSPC,KC_ESC,KC_MINS,
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RGHT,    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
         MO(SYMB),   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                     KC_H,   KC_J,   KC_K,   KC_L,   LT(SYMB, KC_SCLN),KC_QUOT,
-        KC_RBRC,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSPC,
-                    KC_TAB,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_RGUI,
+        KC_RBRC,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
+                    KC_TAB,  KC_TRNS,KC_TRNS,KC_TRNS,          RSFT(KC_0),
         KC_LALT,    CTL_T(KC_ESC),
         MO(MDIA),
         KC_EQL,KC_ENT, KC_SPC
